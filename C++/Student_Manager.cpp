6a8f485f02c793c2498916221c55b7e5d3b0f878
Son <sondt308@gmail.com>
2023-11-05 14:55:20 +0700

    Update file Student_Manager.cpp

--- C++/Student_Manager.cpp
+++ C++/Student_Manager.cpp
@@ -20,7 +20,10 @@ class Student{
         void showStudent();
 
         void setName(string name) {this->name = name;}
-        void setInclass(string course) {this->course = course;}
+        void setAge(int age) {this->age = age;}
+        void setID(int studentID) {this->studentID = studentID;}
+        void setCourse(string course) {this->course = course;}
+        void setSchedule(string classSchedule) {this->classSchedule = classSchedule;}
 };
 
 vector<Student> listStudent;
@@ -47,25 +50,28 @@ void Student::addStudent(){
 }
 
 void Student::deleteStudent(int ID){
-    for (int i=0; i<listStudent.size(); i++)
-    {
-        if (listStudent[i].studentID == ID)
-        {
+    for (int i = 0; i < listStudent.size(); i++){
+        if (listStudent[i].studentID == ID){
             listStudent.erase(listStudent.begin() + i);
         }
     }
 }
 
 void Student::editStudent(int ID){
-    for (int i=0; i<listStudent.size(); i++){
-        if (listStudent[i].studentID == ID)
-        {           
-            string ten, lop;
+    for (int i = 0; i < listStudent.size(); i++){
+        if (listStudent[i].studentID == ID){           
+            string name; int age; int studentID; string course; string classSchedule;
             //cin.ignore();
-            cout << "Nhap lai ten SV: "; getline(cin, ten);
-            cout << "Nhap lai lop SV: "; getline(cin, lop);
-            listStudent[i].setName(ten);
-            listStudent[i].setInclass(lop);
+            cout << "Nhap lai ten SV: "; getline(cin, name);
+            cout << "Nhap lai tuoi: "; cin >> age;
+            cout << "Nhap lai ID: "; cin >> studentID;
+            cout << "Nhap lai lop SV: "; getline(cin, course);
+            cout << "Nhap lai lich hoc: "; getline(cin, classSchedule);
+            listStudent[i].setName(name);
+            listStudent[i].setAge(age);
+            listStudent[i].setID(studentID);
+            listStudent[i].setCourse(course);
+            listStudent[i].setSchedule(classSchedule);
         }
         else
             cout << "MSSV khong ton tai!" << endl;
@@ -79,6 +85,88 @@ void Student::showStudent(){
     cout << "Tuoi: " << listStudent[i].age << endl;
     cout << "Khoa hoc : " << listStudent[i].course << endl;
     cout << "Lich hoc : " << listStudent[i].classSchedule << endl;
+    cout << "-----------" << endl;
+  }
+}
+
+class Lecturer{
+    private: 
+        string name;
+        int age;
+        int lecturerID;
+        string course;
+        string workSchedule;
+    public:
+        Lecturer();
+        Lecturer(string, int, int, string, string);
+        void addLecturer();
+        void editLecturer(int);
+        void deleteLecturer(int);
+        void showLecturer();
+
+        void setName(string name) {this->name = name;}
+        void setAge(int age) {this->age = age;}
+        void setID(int studentID) {this->lecturerID = lecturerID;}
+        void setCourse(string course) {this->course = course;}
+        void setSchedule(string classSchedule) {this->workSchedule = workSchedule;}
+};
+
+vector<Lecturer> listLecturer;
+
+
+Lecturer::Lecturer(string name, int age, int lecturerID, string course, string workSchedule) {
+    this->name = name;
+    this->age = age;
+    this->lecturerID = lecturerID;
+    this->course = course;
+    this->workSchedule = workSchedule;
+}
+
+void Lecturer::addLecturer() {
+    string name; int age; int lecturerID; string course; string workSchedule;
+    cout << "Nhap ten giang vien: "; getline(cin, name);
+    cout << "Nhap tuoi giang vien: "; cin >> age;
+    cout << "Nhap ID giang vien: "; cin >> lecturerID;
+    cout << "Nhap mon day: "; getline(cin, course);
+    cout << "Nhap lich day: "; getline(cin, workSchedule);
+    listLecturer.push_back(Lecturer(name, age, lecturerID, course, workSchedule));
+}
+
+void Lecturer::deleteLecturer(int ID) {
+    for (int i = 0; i < listLecturer.size(); i++){
+        if (listLecturer[i].lecturerID == ID){
+            listLecturer.erase(listLecturer.begin() + i);
+        }
+    }
+}
+
+void Lecturer::editLecturer(int ID) {
+    for(int i = 0; i < listLecturer.size(); i++) {
+        if(listLecturer[i].lecturerID == ID) {
+            string name; int age; int lecturerID; string course; string workSchedule;
+            cout << "Nhap lai ten GV: "; getline(cin, name);
+            cout << "Nhap lai tuoi GV: "; cin >> age;
+            cout << "Nhap lai ID: "; cin >> lecturerID;
+            cout << "Nhap lai mon day: "; getline(cin, course);
+            cout << "Nhap lai lich day: "; getline(cin, workSchedule);
+            listLecturer[i].setName(name);
+            listLecturer[i].setAge(age);
+            listLecturer[i].setID(lecturerID);
+            listLecturer[i].setCourse(course);
+            listLecturer[i].setSchedule(workSchedule);
+        } else {
+            cout << "Khong tim thay ID!" << endl;
+        }
+    }
+}
+
+void Lecturer::showLecturer(){  
+  for (int i = 0; i < listLecturer.size(); i++){
+    cout << "Ho ten: " << listLecturer[i].name << endl;
+    cout << "ID : " << listLecturer[i].lecturerID << endl;
+    cout << "Tuoi: " << listLecturer[i].age << endl;
+    cout << "Mon day : " << listLecturer[i].course << endl;
+    cout << "Lich day : " << listLecturer[i].workSchedule << endl;
     cout << "-----------" << endl;
   }
 }
